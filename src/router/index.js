913368c864663906932df4aee4a7bc8d2851876f
nhandeptraiii <thanhnhan21082003@gmail.com>
2025-03-12 01:09:13 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
   },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
